1c3ea08efacfac5dc9cec843a94accaa5557f13d
Amitav Baruah <abaruah@berkeley.edu>
2018-10-17 14:26:56 -0700

    Simplify file structure and move todo logic to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
 import "./App.css";
-import List from "./List";
+import Item from "./Item";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      curr_item: "",
+      items: []
+    };
+    this.handleItem = this.handleItem.bind(this);
+    this.createList = this.createList.bind(this);
+  }
+  handleItem(event) {
+    this.setState({
+      curr_item: event.target.value
+    });
+  }
+  createList(event) {
+    event.preventDefault()
+    this.setState({
+      curr_item: "",
+      items: this.state.items.concat(this.state.curr_item)
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <List />
+      <div className="todoList">
+        <ul>
+          {this.state.items.map((item, index) => (
+            <Item key={index} text={item} />
+          ))}
+        </ul>
+        <form className="App" onSubmit={this.createList}>
+         <input value={this.state.curr_item} onChange={this.handleItem} />
+         <button>Submit</button>
+       </form>
       </div>
     );
   }
