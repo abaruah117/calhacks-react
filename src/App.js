6bb1b3d87377d38206bd29aed39a3c05d10b0185
trev4ev <trevoraquino@gmail.com>
2018-10-26 16:14:12 -0700

    make skeleton code

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import React, { Component } from "react";
-import "./App.css";
-import Item from "./Item";
+
 class App extends Component {
+
   constructor(props) {
-    super(props);
-    this.state = {
-      curr_item: "",
-      items: []
-    };
-    this.handleItem = this.handleItem.bind(this);
-    this.createList = this.createList.bind(this);
+    super(props)
   }
+
   handleItem(event) {
-    this.setState({
-      curr_item: event.target.value
-    });
+
   }
+
   createList() {
-    if(this.state.curr_item !== ''){
-      this.setState({
-        curr_item: "",
-        items: this.state.items.concat(this.state.curr_item)
-      });
-    }
+
   }
   render() {
-    return (
-      <div className="todo-container">
-        
-        <input className="todo-input" placeholder="New Item" value={this.state.curr_item} onChange={this.handleItem} />
-        <button className="todo-submit" onClick={this.createList}>Submit</button>
-        <ul className="todo-item-list">
-          {this.state.items.map((item, index) => (
-            <Item key={index} text={item} />
-          ))}
-        </ul>
-      </div>
-    );
   }
 }
 
--- src/Item.js
+++ src/Item.js
@@ -2,11 +2,6 @@ import React, { Component } from "react";
 
 class Item extends Component {
   render() {
-    return (
-    	<div className="todo-item-container">
-    		<li className="todo-item"> {this.props.text} </li>
-    	</div>
-    );
   }
 }
 
