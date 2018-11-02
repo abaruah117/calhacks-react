171542e251e27d3919e7a74bd878b27f8e066503
trev4ev <trevoraquino@gmail.com>
2018-11-01 22:44:51 -0700

    change function names

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ class App extends Component {
       items: []
     };
     this.handleItem = this.handleItem.bind(this);
-    this.createList = this.createList.bind(this);
+    this.addItem = this.addItem.bind(this);
   }
   handleItem(event) {
     this.setState({
       curr_item: event.target.value
     });
   }
-  createList() {
+  addItem() {
     if(this.state.curr_item !== ''){
       this.setState({
         curr_item: "",
@@ -29,7 +29,7 @@ class App extends Component {
       <div className="todo-container">
         <div className="input-container">
           <input className="todo-input" placeholder="New Item" value={this.state.curr_item} onChange={this.handleItem} />
-          <button className="todo-submit" onClick={this.createList}>Submit</button>
+          <button className="todo-submit" onClick={this.addItem}>Submit</button>
         </div>
         <ul className="todo-item-list">
           {this.state.items.map((item, index) => (
